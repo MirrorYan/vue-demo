9af94468dfc58841eeaf7efc7dcf060f10321faf
mirror <mirrory@ciandt.com>
2020-10-16 11:44:20 +0800

    Mirror submit.

--- src/views/ComputedWatch/index.vue
+++ src/views/ComputedWatch/index.vue
@@ -1,7 +1,13 @@
 <template>
   <div>
-    
-    <p>正常值：{{ msg }}</p>
+    <el-form label-width="70px">
+      <el-form-item label="正常值">
+        <el-input v-model="msg" width="100" />
+      </el-form-item>
+      <el-form-item label="翻转值">
+        <span>{{ reverseMsg }}</span>
+      </el-form-item>
+    </el-form>
   </div>
 </template>
 
@@ -15,7 +21,7 @@ export default {
   },
   computed: {
     reverseMsg () {
-
+      return this.msg.split('').reverse().join('');
     }
   }
 }

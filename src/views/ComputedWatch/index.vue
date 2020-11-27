072ea883d8a1ba188b4315407499576b85c3b123
mirror <mirrory@ciandt.com>
2020-11-27 18:01:27 +0800

    Daily update.

--- src/views/ComputedWatch/index.vue
+++ src/views/ComputedWatch/index.vue
@@ -2,20 +2,20 @@
   <div v-title :data-title="$route.meta.title">
     <Paragraph>
       <template #title>
-        <h2>计算属性</h2>
+        <h2>计算属性(computed)</h2>
       </template>
-      <p>模板内嵌表达式的初衷是用于简单运算，模板嵌入过多逻辑会让模板过重且难以维护。</p>
-      <p>计算属性用于处理一些复杂逻辑的变量，让代码语义更鲜明。</p>
-      <input type="text" v-model="msg">
+      <p>模板内嵌表达式的初衷是用于简单运算，模板嵌入过多逻辑会让模板过重且难以维护。
+         计算属性用于处理一些复杂逻辑的变量，让代码语义更鲜明。</p>
+      <p>用计算属性实现的文字翻转：</p>
+      <el-form class="form" label-width="70px">
+        <el-form-item label="正常值">
+          <el-input v-model="msg" />
+        </el-form-item>
+        <el-form-item label="翻转值">
+          <span>{{ reverseMsg }}</span>
+        </el-form-item>
+      </el-form>
     </Paragraph>
-    <el-form class="form" label-width="70px">
-      <el-form-item label="正常值">
-        <el-input v-model="msg" />
-      </el-form-item>
-      <el-form-item label="翻转值">
-        <span>{{ reverseMsg }}</span>
-      </el-form-item>
-    </el-form>
   </div>
 </template>
 
@@ -48,5 +48,9 @@ export default {
 <style lang="scss">
 .form {
   max-width: 500px;
+  margin-top: 10px;
+  .el-form-item {
+    margin-bottom: 5px;
+  }
 }
 </style>
